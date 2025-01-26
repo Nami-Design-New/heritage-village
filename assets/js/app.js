966e5d2f1981a2f7c82e74c94a5fd46a2aa88660
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2025-01-26 16:18:32 +0200

    Add preloader functionality, deploy workflow, and SCSS configuration

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $(".preloader").delay(1000).fadeOut(300);
+
+  $("section").each(function () {
+    const sectionDivs = $(this).find("[data-aos]");
+    sectionDivs.each(function (index) {
+      if (!$(this).attr("data-aos-delay")) {
+        $(this).attr("data-aos-delay", (index + 1) * 100);
+      }
+    });
+  });
+
+  AOS.init({
+    offset: 20,
+    delay: 50,
+    duration: 750,
+    once: true,
+  });
+});
